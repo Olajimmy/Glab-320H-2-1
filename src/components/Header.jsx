114897d0f9d4ad46c8576a3106ebf53eedc10471
olajimmy <overdedicated9@gmail.com>
2024-12-12 16:06:22 -0500

    completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
   return (
     <>
-      <h1>Simple React Application</h1>
+      <div className="header">
+        <h1>Simple React Application</h1>
+      </div>
     </>
   );
 }
